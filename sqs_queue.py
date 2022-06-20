899daa741ad1fbf12c74665ff81a3b967ae8f62e
Raj Mani <rajmani@rajs-mbp.myfiosgateway.com>
2022-06-20 13:04:38 -0400

    finished asynchronous queue send and pushes

--- sqs_queue.py
+++ sqs_queue.py
@@ -5,46 +5,93 @@
 import asyncio
 import logging
 import math
+import time
 from typing import Any, List
 
+import uvloop
 from aiobotocore.session import get_session
 from botocore.exceptions import ClientError
-
-from parallel_handler import receive_message
 
 logger = logging.getLogger(__name__)
 
 queue_url = "https://sqs.us-east-1.amazonaws.com/955157183814/test_queue"
 result_queue_url = "https://sqs.us-east-1.amazonaws.com/955157183814/result_queue"
 
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-async def receive_messages(client, queue_name):
-    result = []
+
+async def receive_messages(client, queue_name: str, total_messages: int):
+    print(f"receiving message from {queue_name}")
+    queue = asyncio.Queue()
     try:
         response = await client.get_queue_url(QueueName=queue_name)
         queue_url = response.get("QueueUrl")
-        while True:
+        count = 0
+        while count < total_messages:
             response = await client.receive_message(
-                QueueUrl=queue_url, WaitTimeSeconds=2
+                QueueUrl=queue_url,
+                WaitTimeSeconds=0,
+                MaxNumberOfMessages=10,
             )
             if "Messages" in response:
                 for msg in response["Messages"]:
-                    print(f"Got msg {msg['Body']}")
-                    result.append(msg["Body"])
+                    await queue.put(msg["Body"])
                     await client.delete_message(
                         QueueUrl=queue_url, ReceiptHandle=msg["ReceiptHandle"]
                     )
+                    count += 1
             else:
                 print("No Messages in Queue")
-                break
+                continue
     except Exception as e:
         print(e)
     else:
-        return result
+        # purging the queue to ensure all the messages are deleted
+        # client.purge_queue(QueueUrl=queue_url)
+        return queue
 
 
-async def send_batch_message(client, queue_name):
-    pass
+async def send_batch_message(client, queue_name, batch):
+    print(f"sending message to queue {queue_name} with batch {batch}")
+    try:
+        response = await client.get_queue_url(QueueName=queue_name)
+    except ClientError as err:
+        if err.response["Error"]["Code"] == "AWS.SimpleQueueService.NonExistentQueue":
+            print(f"Queue {queue_name} does not exist")
+        else:
+            raise
+    else:
+        queue_url = response["QueueUrl"]
+        try:
+            entries = [
+                {
+                    "Id": str(ind),
+                    "MessageBody": msg["body"],
+                }
+                for ind, msg in enumerate(batch)
+            ]
+            response = await client.send_message_batch(
+                QueueUrl=queue_url, Entries=entries
+            )
+            if "Successful" in response:
+                for msg_meta in response["Successful"]:
+                    logger.info(
+                        "Message sent: %s: %s",
+                        msg_meta["MessageId"],
+                        batch[int(msg_meta["Id"])]["body"],
+                    )
+            elif "Failed" in response:
+                for msg_meta in response["Failed"]:
+                    logger.warning(
+                        "Failed to send: %s: %s",
+                        msg_meta["MessageId"],
+                        batch[int(msg_meta["Id"])]["body"],
+                    )
+        except ClientError as err:
+            logger.exception(f"Send messages failed to queue: {queue_name}")
+            raise err
+        else:
+            return response
 
 
 def batch_records(records: List[Any], batch_size: int) -> List[List[Any]]:
@@ -57,9 +104,10 @@ def batch_records(records: List[Any], batch_size: int) -> List[List[Any]]:
 
 
 async def send_and_receive(client, batch):
-    await send_batch_message(client, "test_queue")
-    result = await receive_message()
-    return result
+    res = await send_batch_message(client, "test_queue", batch)
+    return res
+    # result = await receive_messages(client, "result_queue")
+    # return result
 
 
 async def handler(event, context=None):
@@ -97,13 +145,24 @@ async def handler(event, context=None):
         session = get_session()
         async with session.create_client("sqs", region_name="us-east-1") as client:
             tasks = [
-                asyncio.create_task(send_and_receive(client, batcn))
-                for batcn in batch_records(messages, 10)
+                asyncio.create_task(send_batch_message(client, "test_queue", batch))
+                for batch in batch_records(messages, 10)
             ]
             result = await asyncio.gather(*tasks)
             print(result)
 
+            result = await receive_messages(
+                client,
+                "result_queue",
+                len(messages),
+            )
+            print(result)
+            return result
+
 
 if __name__ == "__main__":
-
-    session = get_session()
+    start = time.perf_counter()
+    event = {"action": "test-queue-process"}
+    asyncio.run(handler(event, None))
+    end = time.perf_counter()
+    print(f"program finished in {(end-start):.4f} seconds")
